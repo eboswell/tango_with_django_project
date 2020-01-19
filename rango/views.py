faae52e5c873a2f4dc929049667a57a67b49b4a6
Elizabeth Boswell <2398840B@student.gla.ac.uk>
2020-01-19 16:58:02 +0000

    after chapter 3 tests, changed urls.py and views.py

--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says here there partner")
+    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
+
+def about(index):
+    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
